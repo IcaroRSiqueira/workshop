b91edd09a605898b665ea7f6a78a75ad6c5468a1
Icaro <icaro@IcaroMacBook-Pro.local>
2020-03-04 17:24:51 -0300

    Finalizada feature de desafios
    Co-authored-by: andrecego <andre270492@gmail.com>

--- spec/features/user_send_result_to_challenge_spec.rb
+++ spec/features/user_send_result_to_challenge_spec.rb
@@ -21,15 +21,27 @@ feature 'User send result to challenge' do
     challenge = create(:challenge, title: 'Desafio 01', 
                                    description: 'Quanto é 10 + 10')
     user = create(:user)
-    create(:solution, challenge: challenge, user: user)
+    solution = create(:solution, challenge: challenge, user: user)
     login_as user
 
-    visit challenge_path(challenge)
-    click_on 'Responder'
+    visit new_challenge_solution_path(challenge, solution)
     fill_in 'Resultado', with: '20'
     click_on 'Enviar'
 
     expect(page).to have_content('Não foi possível cadastrar sua resposta')
     expect(page).to have_content('Usúario já respondeu')
+  end
+
+  scenario 'already had sent one solution' do
+    challenge = create(:challenge, title: 'Desafio 01', 
+                                   description: 'Quanto é 10 + 10')
+    user = create(:user)
+    create(:solution, challenge: challenge, user: user)
+    login_as user
+
+    visit challenge_path(challenge)
+
+    expect(page).not_to have_link('Responder')
+    expect(page).to have_content('Você já respondeu esse desafio.')
   end
 end
